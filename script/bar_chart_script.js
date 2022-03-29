c7b95b6e5a06f73efee475598a5e8e2e87325e4c
Jennifer Manwell <manwell.fortier@gmail.com>
2022-03-29 19:18:53 -0400

    functional bar chart, minimal styles applied

--- script/bar_chart_script.js
+++ script/bar_chart_script.js
@@ -1,66 +1,139 @@
-const errorChecks = function() {
+//helper function to detect missing input items
+const barChartParamsMissing = function() {
+  let txtBarChartTitle = $("#txtBarChartTitle").val();
+  let txtBarChartUpper = $("#txtBarUpperLimit").val();
+  let txtBarChartLower = $("#txtBarLowerLimit").val();
+  let txtBarDType = $("#txtBarDType").val();
+
+  const arrMissingItems = [];
+  let errorFlag = false;
+  if(txtBarChartTitle === ""){
+    arrMissingItems.push("title");
+    errorFlag=true;
+  }
+  if(txtBarChartUpper === ""){
+    arrMissingItems.push("upper limit");
+    errorFlag=true;
+  }
+  if(txtBarChartLower === ""){
+    arrMissingItems.push("lower limit");
+    errorFlag=true;
+  }
+  if(txtBarDType === ""){
+    arrMissingItems.push("data type");
+    errorFlag=true;
+  }
+  if(errorFlag){
+    alert("Please enter the following items: \n\n" + arrMissingItems.join(", "));
+  }
+  return errorFlag;
+}
+
+//helper function to detect error in input for bar chart table
+const barChartTableParamsMissing = function() {
+
+  let bTitle = $("#txtBarTitle").val();
+  let bValue = $("#txtBarValue").val();
+  const arrMissingItems = [];
+  let errorFlag = false;
+
+  if (bTitle === "" ){
+    arrMissingItems.push(bTitle);
+    errorFlag=true;
+  }
+  if(bValue === ""){
+    arrMissingItems.push(bTitle);
+    errorFlag=true;
+  }
+  if(errorFlag){
+    alert("Please enter the following items: \n\n" + arrMissingItems.join(", "));
+  }
+  return errorFlag;
+
 
 }
+//helper function - error check to ensure value entered is not < lower or > upper limits
+
+const upperLowerLimitError = function() {
+  let txtValue = $("#txtBarValue").val();
+  let txtBarChartUpper = $("#txtBarUpperLimit").val();
+  let txtBarChartLower = $("#txtBarLowerLimit").val();
+  let errorFlag = false;
+
+  if(txtValue < txtBarLowerLimit) {
+    errorFLag = true;
+    alert("The bar value must be greater than the lower limit of the chart.")
+  }if(txtValue > txtBarLowerLimit) {
+    errorFLag = true;
+    alert("The bar value must be lower than the upper limit of the chart.")
+  }
+  return errorFlag;
+}
+
+//calculate the width of each table column
 const calcBarWeight = function(lowerLimit, upperLimit)
 {
   let span = upperLimit - lowerLimit;
-  let weight = span / 100;
+  let weight = span / 300;
 
   return weight;
 }
+
+
 //Generate a bar chart using the items entered by the user
 $(document).ready(function() {
   $("#btnChartMe").click(function(){
+    let tdLineFiller = "", trOpen= "", thLine = "", tdLine = "", tdBlankLine = "", barWeight = "";
+    let txtBarChartTitle = $("#txtBarChartTitle").val();
+    let txtBarChartUpper = $("#txtBarUpperLimit").val();
+    let txtBarChartLower = $("#txtBarLowerLimit").val();
+    let txtBarDType = $("#txtBarDType").val();
 
-    let bBarChartTitle = $("#txtBarChartTitle").val();
-    let bBarChartUpper = $("#txtBarUpperLimit").val();
-    let bBarChartLower = $("#txtBarLowerLimit").val();
-    let tdLineFiller, trOpen, thLine, tdLine, tdBlankLine = "";
     let tableBody = $("#myChart tbody:last");
+    let tableRows = 0, prevHeight = 0, divInterval = 0, gridCount = 0;
 
-    if(bBarChartTitle === ""){
-      alert("PLease enter a title for the bar chart.")
-    }else if(bBarChartUpper === ""){
-      alert("Please enter an upper limit for the bar chart");
-    }else if(bBarChartLower === ""){
-      alert("Please enter a lower limit for the bar chart")
-    }else {
-     // alert($("#myChart thDType").val())
-     // $("#myChart thDType").val($("#txtBarDType").val());
-      let barWeight = calcBarWeight($("#txtBarLowerLimit").val(), $("#txtBarUpperLimit").val())
-      let arrTitleRange = $("#txtBarTitles").val().split(",");
-      let arrValueRange = $("#txtBarValues").val().split(",");
-      for(let y = 0; y < 100; y ++){
-        tdLineFiller = tdLineFiller + "<td></td>";
-      }
-      tableBody.append("<tr style=visibility:collapse>" + tdLineFiller + "</tr>");
+    barWeight = calcBarWeight($("#txtBarLowerLimit").val(), $("#txtBarUpperLimit").val())
 
+    const arrTitleRange = $("#txtBarTitles").val().split(",");
+    const arrValueRange = $("#txtBarValues").val().split(",");
 
+    tableRows = arrValueRange + 1;
+    tdLine = "<tr height=300px><th id=gridTh>";
+    let divText = txtBarChartUpper;
+    for(let y = 1;y < 6 ; y++) {
 
-      //alert("here");
-      for(let i=0; i < arrTitleRange.length;i++){
-//        trOpen = "<tr class=bar colspan=" + arrValueRange[i] + $("#txtBarDType").val() + ">";
-        trOpen = "<tr>";
-        thLine = "<th class=sideTitle scope=row>" + arrTitleRange[i] + "</th>";
-        tdLine = "<td class=bar colspan =" +  arrValueRange[i]/barWeight + ">" + arrValueRange[i] + "%</span></td>";
-        if(arrValueRange[i] < bBarChartUpper){
-          tdBlankLine = "<td colspan=" + ((bBarChartUpper-arrValueRange[i])/barWeight) + "></td></tr>";
-        }else {
-          tdBlankLine = "</tr>";
-        }
+        divText -= divInterval;
+        divInterval = (txtBarChartUpper - txtBarChartLower) / 5;
+        let divHeight = 300/5;
 
-
-        tableBody.append(trOpen + thLine + tdLine + tdBlankLine);
-
-      }
-    $("#txtBarTitles").val("");
-    $("#txtBarValues").val("");
+        tdLine+= "<div style = \"height: " + divHeight + "px\">" + divText + "</div>";
     }
+    tdLine += "</th>"
+
+
+    for(let i=0; i < arrTitleRange.length;i++){
+
+      if(i < 10){
+        gridCount = i;
+      }else {
+        gridCount = i - 10;
+      }
+      tdLine += "<td id=gridTd><div id=gridDiv" + gridCount + " style = \"height: " +  arrValueRange[i]/barWeight + "px;\">" + arrValueRange[i] + txtBarDType + "</div></td>";
+    }
+    tdLine += "</tr>";
+    tableBody.append(tdLine);
+
+    thLine = "<tr><td>values</td>";
+    for(let i = 0; i < arrTitleRange.length; i++){
+      thLine += "<td>" + arrTitleRange[i] + "</td>";
+    }
+    thLine += "</tr>";
+    tableBody.append(thLine);
   });
 });
 
 
-//Process user input for the bar chart
+//Append new bar chart items to ones already entered
 $(document).ready(function() {
   $("#btnAddItem").click(function(){
 
@@ -69,17 +142,12 @@ $(document).ready(function() {
     let bTitleRange = $("#txtBarTitles").val();
     let bValueRange = $("#txtBarValues").val();
 
-    if(flagFieldsValid === 'true')
-    {
-      if(bTitle === "" && bValue === ""){
-        alert("Please enter values for both title and range value");
-       }else if (bTitle === "" ){
-        alert("Please enter values for title");
-       }else if(bValue === ""){
-        alert("Please enter values for range");
-       }else if(isNaN(bValue)) {
+
+    if(!barChartTableParamsMissing() && !upperLowerLimitError()){
+
+      if(isNaN(bValue)) {
         alert('The range value must be a number');
-        $("#txtBarValue").val("");
+        //$("#txtBarValue").val("");
       }else {
         if(bTitleRange === ""){
           $("#txtBarTitles").val(bTitle);
@@ -95,9 +163,8 @@ $(document).ready(function() {
           $("#txtBarValues").val($("#txtBarValues").val()  + "," + bValue);
           $("#txtBarValue").val("");
         }
-       }
+      }
     }
-
   });
 });
 
